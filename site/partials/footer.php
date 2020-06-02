55cb6021eeae33c24059b841bc6e9061d30ddfaf
imritik <ritikvverma@gmail.com>
2020-06-02 17:23:32 +0530

    vendor dashboard in progress

--- site/partials/footer.php
+++ site/partials/footer.php
@@ -26,51 +26,51 @@
  
   
     <!-- Modernizr Plugin -->
-    <script src="js/modernizr.custom.79639.js"></script>
+    <script src="../js/modernizr.custom.79639.js"></script>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-1.11.2.min.js"></script>
+    <!-- jQuery (../necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery-1.11.2.min.js"></script>
 
-    <!-- Bootstrap Plugins -->
-    <script src="js/bootstrap.min.js"></script>
+    <!-- Bootstra../p Plugins -->
+    <script src="../js/bootstrap.min.js"></script>
 
-    <!-- Retina Plugin -->
-    <script src="js/retina.min.js"></script>
+    <!-- Retina P../lugin -->
+    <script src="../js/retina.min.js"></script>
 
-    <!-- ScrollReveal Plugin -->
-    <script src="js/scrollReveal.min.js"></script>
+    <!-- ScrollRe../veal Plugin -->
+    <script src="../js/scrollReveal.min.js"></script>
 
-    <!-- Flex Menu Plugin -->
-    <script src="js/jquery.flexmenu.js"></script>
+    <!-- Flex Men../u Plugin -->
+    <script src="../js/jquery.flexmenu.js"></script>
 
-    <!-- Slider Plugin -->
-    <script src="js/jquery.ba-cond.min.js"></script>
-    <script src="js/jquery.slitslider.js"></script>
+    <!-- Slider P../lugin -->
+    <script src="../js/jquery.ba-cond.min.js"></script>
+    <script src="../js/jquery.slitslider.js"></script>
 
-    <!-- Carousel Plugin -->
-    <script src="js/owl.carousel.min.js"></script>
+    <!-- Carousel../ Plugin -->
+    <script src="../js/owl.carousel.min.js"></script>
 
-    <!-- Parallax Plugin -->
-    <script src="js/parallax.js"></script>
+    <!-- Parallax../ Plugin -->
+    <script src="../js/parallax.js"></script>
 
-    <!-- Counterup Plugin -->
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
+    <!-- Counteru../p Plugin -->
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
 
-    <!-- No UI Slider Plugin -->
-    <script src="js/jquery.nouislider.all.min.js"></script>
+    <!-- No UI Sl../ider Plugin -->
+    <script src="../js/jquery.nouislider.all.min.js"></script>
 
-    <!-- Bootstrap Wysiwyg Plugin -->
-    <script src="js/bootstrap3-wysihtml5.all.min.js"></script>
+    <!-- Bootstra../p Wysiwyg Plugin -->
+    <script src="../js/bootstrap3-wysihtml5.all.min.js"></script>
 
-    <!-- Flickr Plugin -->
-    <script src="js/jflickrfeed.min.js"></script>
+    <!-- Flickr P../lugin -->
+    <script src="../js/jflickrfeed.min.js"></script>
 
-    <!-- Fancybox Plugin -->
-    <script src="js/fancybox.pack.js"></script>
+    <!-- Fancybox../ Plugin -->
+    <script src="../js/fancybox.pack.js"></script>
 
-    <!-- Magic Form Processing -->
-    <script src="js/magic.js"></script>
+    <!-- Magic Fo../rm Processing -->
+    <script src="../js/magic.js"></script>
 
-    <!-- jQuery Settings -->
-    <script src="js/settings.js"></script>
+    <!-- jQuery S../ettings -->
+    <script src="../js/settings.js"></script>
